df0ca3cebad1bdd33536c64e762cd4d6535cc35a
Jon Hagg <jon@breakthroughenergy.org>
2021-12-15 14:55:48 -0800

    chore: add back type hints

--- fs/azblob/blob_file.py
+++ fs/azblob/blob_file.py
@@ -1,6 +1,7 @@
 import io
 import os
 import tempfile
+from typing import Optional
 
 from fs.mode import Mode
 
@@ -16,11 +17,11 @@ class BlobFile(io.IOBase):
         return proxy
 
     def __repr__(self):
-        return f"BlobFile({self.blob.blob_name}, {self.mode})"
+        return f"BlobFile({self._blob.blob_name}, {self.mode})"
 
     def __init__(self, f, blob, mode):
         self._f = f
-        self.blob = blob
+        self._blob = blob
         self.mode = Mode(mode)
 
     def __enter__(self):
@@ -33,70 +34,70 @@ class BlobFile(io.IOBase):
     def raw(self):
         return self._f
 
-    def close(self):
+    def close(self) -> None:
         if self.mode.writing:
             self.seek(0)
-            self.blob.upload_blob(self.raw, overwrite=True)
+            self._blob.upload_blob(self.raw, overwrite=True)
         self._f.close()
 
     @property
-    def closed(self):
+    def closed(self) -> bool:
         return self._f.closed
 
-    def fileno(self):
+    def fileno(self) -> int:
         return self._f.fileno()
 
-    def flush(self):
+    def flush(self) -> None:
         return self._f.flush()
 
-    def readable(self):
+    def readable(self) -> bool:
         return self.mode.reading
 
-    def readline(self, limit=-1):
+    def readline(self, limit: Optional[int] = -1) -> bytes:
         return self._f.readline(limit)
 
-    def seek(self, offset, whence=os.SEEK_SET):
+    def seek(self, offset: int, whence: int = os.SEEK_SET) -> int:
         if whence not in (os.SEEK_CUR, os.SEEK_END, os.SEEK_SET):
             raise ValueError("invalid value for 'whence'")
         self._f.seek(offset, whence)
         return self._f.tell()
 
-    def seekable(self):
+    def seekable(self) -> bool:
         return True
 
-    def tell(self):
+    def tell(self) -> int:
         return self._f.tell()
 
-    def writable(self):
+    def writable(self) -> bool:
         return self.mode.writing
 
-    def writelines(self, lines):
+    def writelines(self, lines) -> None:
         return self._f.writelines(lines)
 
-    def read(self, n=-1):
+    def read(self, n: int = -1) -> bytes:
         if not self.mode.reading:
             raise OSError("not open for reading")
         return self._f.read(n)
 
-    def readall(self):
+    def readall(self) -> bytes:
         return self._f.read()
 
-    def readinto(self, b):
+    def readinto(self, b) -> int:
         return self._f.readinto(b)
 
-    def write(self, b):
+    def write(self, b) -> int:
         if not self.mode.writing:
             raise OSError("not open for writing")
         self._f.write(b)
         return len(b)
 
-    def truncate(self, size=None):
+    def truncate(self, size: Optional[int] = None) -> int:
         if size is None:
             size = self._f.tell()
         self._f.truncate(size)
         return size
 
-    def __next__(self):
+    def __next__(self) -> bytes:
         line = self._f.readline()
         if not line:
             raise StopIteration
