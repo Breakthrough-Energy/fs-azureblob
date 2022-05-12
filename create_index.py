5c78437091b82a86895570a3841033c42dd8dc4b
Jon Hagg <jon@breakthroughenergy.org>
2022-05-11 18:17:59 -0700

    fixes and add links

--- create_index.py
+++ create_index.py
@@ -12,21 +12,32 @@ def open_container(container):
 bfs = open_container(container)
 
 
-def _gen_index(bfs, ul):
+def _url(path, name):
+    base = f"https://besciences.blob.core.windows.net/{container}"
+    if path != "":
+        base += f"{path}"
+    link = f"{base}/{name}"
+    return f'<a href="{link}">{name}</a>'
+
+
+def _gen_index(bfs, ul, path):
     ul.append("<ul class='nested'>")
     for f in bfs.walk.files(max_depth=1):
-        ul.append(f"<li>{bfs.getinfo(f).name}</li>")
+        href = _url(path, bfs.getinfo(f).name)
+        ul.append(f"<li>{href}</li>")
     for f in bfs.walk.dirs(max_depth=1):
         name = bfs.getinfo(f).name
-        ul.append(f"<li><span class='carat'>{name}</span>")
-        _gen_index(bfs.opendir(f), ul)
+        ul.append(f"<li><span class='caret'>{name}</span>")
+        _gen_index(bfs.opendir(f), ul, f"{path}{f}")
         ul.append("</li>")
     ul.append("</ul>")
 
 
 def gen_index(bfs):
     ul = ["<ul id='myUL'>"]
-    _gen_index(bfs, ul)
+    ul.append(f"<li><span class='caret'>{container}</span>")
+    _gen_index(bfs, ul, "")
+    ul.append("</li>")
     ul.append("</ul>")
     return ul
 
@@ -34,12 +45,13 @@ def gen_index(bfs):
 ul = gen_index(bfs)
 
 with open("index.html", "w") as f:
+    f.write("<!DOCTYPE html>\n")
     f.write("<html>\n")
     f.write("<head>\n")
-    f.write("<link rel='stylesheet' href='index.css'>\n")
-    f.write("<script src='index.js'></script>\n")
+    f.write('<link rel="stylesheet" href="index.css">\n')
     f.write("</head>\n")
     f.write("<body>\n")
     f.writelines(line + "\n" for line in ul)
     f.write("</body>\n")
+    f.write('<script src="index.js"></script>\n')
     f.write("</html>\n")
