c370ca76102ecbdaab2d374dfcc1ac3d62f0069a
Jen Hagg <jen@breakthroughenergy.org>
2022-06-17 17:57:36 -0700

    feat: wip gen2 datalake fs

---
+++ fs/azblob/blob_fs_v2.py
@@ -0,0 +1,209 @@
+import datetime
+from typing import Any, BinaryIO
+
+from azure.storage.filedatalake import DataLakeServiceClient
+from fs.base import FS
+from fs.enums import ResourceType
+from fs.info import Info
+from fs.mode import Mode
+from fs.path import dirname
+from fs.subfs import SubFS
+from fs.time import datetime_to_epoch
+
+from fs import errors
+from fs.azblob.blob_file import BlobFile
+from fs.azblob.const import (
+    ACCESSED,
+    BASIC,
+    CREATED,
+    CREATION_TIME,
+    DETAILS,
+    INVALID_CHARS,
+    IS_DIR,
+    LAST_ACCESSED_ON,
+    LAST_MODIFIED,
+    METADATA_CHANGED,
+    MODIFIED,
+    NAME,
+    SIZE,
+    TYPE,
+)
+from fs.azblob.error_tools import blobfs_errors
+
+
+def _convert_to_epoch(props: dict) -> None:
+    for k, v in props.items():
+        if isinstance(v, datetime.datetime):
+            props[k] = datetime_to_epoch(v)
+
+
+def _is_dir(blob):
+    metadata = blob["metadata"]
+    if metadata is not None:
+        if "hdi_isfolder" in metadata:
+            if metadata["hdi_isfolder"] == "true":
+                return True
+    return False
+
+
+def _info_from_dict(props: dict, namespaces) -> Info:
+    info = {BASIC: {NAME: props[NAME], IS_DIR: _is_dir(props)}}
+    if DETAILS in namespaces:
+        details = {
+            ACCESSED: props[LAST_ACCESSED_ON],
+            CREATED: props[CREATION_TIME],
+            METADATA_CHANGED: None,
+            MODIFIED: props[LAST_MODIFIED],
+            SIZE: props[SIZE],
+        }
+        if info[BASIC][IS_DIR]:
+            info[DETAILS][TYPE] = ResourceType.directory
+        else:
+            info[DETAILS][TYPE] = ResourceType.file
+
+        _convert_to_epoch(details)
+        info[DETAILS] = details
+    return Info(info)
+
+
+class BlobFSV2(FS):
+    _meta = {"invalid_path_chars": INVALID_CHARS}
+
+    def __init__(self, account_name: str, container: str, account_key=None):
+        super().__init__()
+        self._svc = DataLakeServiceClient(
+            account_url=f"https://{account_name}.dfs.core.windows.net",
+            credential=account_key,
+        )
+        self.client = self._svc.get_file_system_client(container)
+        self._check_container_client()
+
+    def _check_container_client(self):
+        try:
+            if not self.client.exists():
+                raise errors.CreateFailed("Container does not exist")
+        except:  # noqa
+            raise errors.CreateFailed(
+                "Invalid parameters. Either incorrect account details, or container does not exist"
+            )
+
+    def getinfo(self, path: str, namespaces=None) -> Info:
+        self.check()
+        namespaces = namespaces or ()
+        path = self._validatepath(path)
+
+        blob = self.client.get_file_client(path)
+        if not blob.exists():
+            raise errors.ResourceNotFound(path)
+
+        props = dict(blob.get_file_properties())
+        return _info_from_dict(props, namespaces)
+
+    def listdir(self, path: str) -> list:
+        self.check()
+        path = self._validatepath(path)
+        if not self.getinfo(path).is_dir:
+            raise errors.DirectoryExpected(path)
+        paths = self.client.get_paths(path, recursive=False)
+        return [p["name"].split("/")[-1] for p in paths]
+
+    def openbin(
+        self, path: str, mode: str = "r", buffering: int = -1, **options: Any
+    ) -> BinaryIO:
+        self.check()
+        path = self._validatepath(path)
+        _mode = Mode(mode)
+
+        self._check_mode(path, _mode)
+        self._check_dir_path(path)
+        blob = self.client.get_blob_client(path)
+        blob_file = BlobFile.factory(blob, _mode.to_platform_bin())
+
+        if self.exists(path):
+            stream = blob.download_blob()
+            stream.readinto(blob_file.raw)
+
+        if _mode.truncate:
+            blob_file.seek(0)
+            blob_file.truncate()
+        elif not _mode.appending:
+            blob_file.seek(0)
+
+        return blob_file  # type: ignore
+
+    def _check_dir_path(self, path: str) -> None:
+        """Ensure the parent directory of path exists"""
+        try:
+            dir_path = dirname(path)
+            self.getinfo(dir_path)
+        except errors.ResourceNotFound:
+            raise errors.ResourceNotFound(path)
+
+    def _check_mode(self, path: str, mode: Mode) -> None:
+        """Ensure path can be opened using the given mode"""
+        mode.validate_bin()
+        try:
+            info = self.getinfo(path)
+            if mode.exclusive:
+                raise errors.FileExists(path)
+            if info.is_dir:
+                raise errors.FileExpected(path)
+        except errors.ResourceNotFound:
+            if not mode.create:
+                raise errors.ResourceNotFound(path)
+
+    def _validatepath(self, path: str) -> str:
+        _path = super().validatepath(path)
+        if _path.endswith("."):
+            raise errors.InvalidPath(path)
+
+        return _path.strip("/")
+
+    def _check_makedir(self, path: str, recreate: bool) -> None:
+        if not self.isdir(dirname(path)):
+            raise errors.ResourceNotFound(path)
+        if not recreate:
+            if path == "" or self.exists(path):
+                raise errors.DirectoryExists(path)
+
+    def makedir(self, path: str, permissions=None, recreate: bool = False) -> SubFS:  # type: ignore
+        self.check()
+        path = self._validatepath(path)
+        self._check_makedir(path, recreate)
+        self.client.create_directory(path)
+        return SubFS(self, path)
+
+    def remove(self, path: str) -> None:
+        self.check()
+        path = self._validatepath(path)
+        if self.getinfo(path).is_dir:
+            raise errors.FileExpected(path)
+        with blobfs_errors(path):
+            self.client.delete_file(path)
+
+    def removedir(self, path: str) -> None:
+        self.check()
+        _path = self._validatepath(path)
+        if _path == "":
+            raise errors.RemoveRootError()
+        info = self.getinfo(_path)
+        if not info.is_dir:
+            raise errors.DirectoryExpected(path)
+        if not self.isempty(path):
+            raise errors.DirectoryNotEmpty(path)
+        self.client.delete_directory(path)
+
+    def setinfo(self, path: str, info) -> None:
+        self.check()
+        path = self._validatepath(path)
+        if not self.exists(path):
+            raise errors.ResourceNotFound(path)
+        if DETAILS in info:
+            details = info[DETAILS]
+            meta = {
+                LAST_ACCESSED_ON: str(details[ACCESSED]),
+                LAST_MODIFIED: str(details[MODIFIED]),
+            }
+            with blobfs_errors(path):
+                blob = self.client.get_blob_client(path)
+                blob.set_blob_metadata(meta)
--- fs/opener/blob_fs.py
+++ fs/opener/blob_fs.py
@@ -1,6 +1,7 @@
 __all__ = ["BlobFSOpener"]
 
 from fs.azblob import BlobFS
+from fs.azblob.blob_fs_v2 import BlobFSV2
 from fs.opener import Opener
 from fs.opener.errors import OpenerError
 
@@ -23,3 +24,23 @@ class BlobFSOpener(Opener):
             account_key = None
 
         return BlobFS(account_name, container, account_key)
+
+
+class BlobFSV2Opener(Opener):
+    """Open url at azblobv2://<account_name>:<account_key>@<container>"""
+
+    protocols = ["azblobv2"]
+
+    def open_fs(self, fs_url, parse_result, writeable, create, cwd):
+        account_name = parse_result.username
+        account_key = parse_result.password
+        container = parse_result.resource
+
+        if account_name is None:
+            raise OpenerError("account_name is required")
+        if container is None:
+            raise OpenerError("container is required")
+        if account_key == "":
+            account_key = None
+
+        return BlobFSV2(account_name, container, account_key)
