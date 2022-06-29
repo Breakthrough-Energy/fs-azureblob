31c461772d1684794921ab238070422e8bd6cc52
Jen Hagg <jen@breakthroughenergy.org>
2022-06-29 16:05:30 -0700

    test: run base test cases for v2 fs

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,18 @@ import unittest
 import pytest
 from fs.test import FSTestCases
 
-from fs.azblob import BlobFS
+from fs.azblob import BlobFS, BlobFSV2
+
+
+@pytest.mark.creds
+class TestBlobFSV2(FSTestCases, unittest.TestCase):
+    def make_fs(self):
+        account_name = "esmi"
+        container = "test3"
+        key = os.getenv("BLOB_ACCOUNT_KEY_V2")
+        bfs = BlobFSV2(account_name, container, key)
+        bfs.removetree("/")
+        return bfs
 
 
 @pytest.mark.creds
