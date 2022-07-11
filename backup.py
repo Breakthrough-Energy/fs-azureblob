751d6756d3e2536ba3456abfc05f85e611931e10
Jen Hagg <jen@breakthroughenergy.org>
2022-07-11 15:24:43 -0700

    refactor: encapsulate workflow per directory

--- backup.py
+++ backup.py
@@ -1,7 +1,6 @@
 import fs
-import csv
 import os
-from fs.copy import copy_dir_if
+from fs.copy import copy_dir_if, copy_file
 from fs.wrap import read_only
 import logging
 
@@ -13,18 +12,8 @@ container = "scenariodata"
 key = os.environ["BLOB_ACCOUNT_KEY_V2"]
 
 
-lfs = read_only(fs.open_fs("/mnt/bes/pcm"))
 bfs = fs.open_fs(f"azblobv2://{account}:{key}@{container}")
-
-
-def list_ids(_fs):
-    path = "ScenarioList.csv"
-    ids = []
-    with _fs.open(path) as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            ids.append(row["id"])
-    return ids
+lfs = read_only(fs.open_fs("/mnt/bes/pcm"))
 
 
 def _on_copy(src_fs, src_path, dst_fs, dst_path):
@@ -38,29 +27,38 @@ def _size(_fs, path):
     if not _fs.exists(path):
         return None
     try:
-        return _fs.getinfo(path, namespaces=["details"]).size
+        return _fs.getsize(path)
     except Exception as e:
         print(e)
         return -1
 
 
-def mismatch(path):
-    fs1 = lfs.opendir(path)
-    fs2 = bfs.opendir(path)
-    for _path in fs1.walk.files():
-        s1 = _size(fs1, _path)
-        s2 = _size(fs2, _path)
-        if s2 is None or s1 != s2:
-            print(f"{_path=}, {s1=}, {s2=}")
+class Backup:
+    def __init__(self, path):
+        self.path = path
+        self.fs1 = lfs.opendir(path)
+        self.fs2 = bfs.opendir(path)
 
+    def _mismatch(self):
+        result = []
+        for _path in self.fs1.walk.files():
+            s1 = _size(self.fs1, _path)
+            s2 = _size(self.fs2, _path)
+            if s2 is None or s1 != s2:
+                print(f"{_path=}, {s1=}, {s2=}")
+                result.append(_path)
+        return result
 
-def main():
-    path = "data/output"
-    fs1 = lfs.opendir(path)
-    fs2 = bfs.opendir(path)
-    copy_dir_if(fs1, ".", fs2, ".", condition="newer", on_copy=_on_copy)
+    def fix_missing(self):
+        for p in self._mismatch():
+            copy_file(self.fs1, p, self.fs2, p)
+
+    def main(self):
+        copy_dir_if(self.fs1, ".", self.fs2, ".", condition="newer", on_copy=_on_copy)
 
 
 if __name__ == "__main__":
-    mismatch("data/input")
-    # mismatch("data/output")
+    path = "data/output"
+    backup = Backup(path)
+    # backup.fix_missing()
+    backup.main()
